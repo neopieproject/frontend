8e3bc6a8a5d29e20a2f0f0a073b430ac3ab3e98e
neopie <neopieproject@gmail.com>
2024-11-14 23:15:10 -0800

    Update neoline.ts to check for N3 MainNet network

--- src/utils/neo/wallets/neoline.ts
+++ src/utils/neo/wallets/neoline.ts
@@ -2,11 +2,11 @@ export const initNeoLine = async () => {
   try {
     // @ts-ignore
     const instance = new NEOLineN3.Init();
-    // NEOLineN3 doesn't have getNetworks function
-    // @ts-ignore
-    const instance2 = new NEOLine.Init();
-    const network = await instance2.getNetworks();
+    const network = await instance.getNetworks();
     const account = await instance.getAccount();
+    if (network.defaultNetwork !== "N3MainNet") {
+      throw new Error("Switch your network to N3 MainNet.");
+    }
     return {
       name: "NeoLine",
       instance,
